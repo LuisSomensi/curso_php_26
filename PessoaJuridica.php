27aadf94ac8f7e14ef6b2f33d1dc5513077b5fdb
suporte <suporte@BG-LABX-PCX.localdomain>
2026-01-13 21:54:59 -0300

    CPF e CNPJ

--- PessoaJuridica.php
+++ PessoaJuridica.php
@@ -4,8 +4,9 @@ require_once "./PessoaAbstract.php";
 
 define("QUEBRAR_LINHA", "<br>");
 
-class PessoaJuridica extends PessoaAbstract{}
+class PessoaJuridica extends PessoaAbstract{
     public $CNPJ;
+
     public function __constrct() {
         $this->resetarPessoa();
         $this->CNPJ = "";
@@ -14,7 +15,6 @@ class PessoaJuridica extends PessoaAbstract{}
         $cpf = $this->CNPJ;
 
         $this->validarCPF($cnpj);
-    }
-    }
-
 }
+}
+
